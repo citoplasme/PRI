9959a9855845679ea2e54cf756a501b3bc39341e
João Pimentel <32567686+citoplasme@users.noreply.github.com>
2019-10-22 10:58:45 +0100

    Update musica.js

--- TPC4/musica.js
+++ TPC4/musica.js
@@ -2,27 +2,25 @@ var http = require('http')
 var fs = require('fs')
 
 var myserver = http.createServer(function(req,res) {
-    var partes = req.url.split('/')
-    var pag = partes[partes.length-1]
+    var spl = req.url.split('/')
+    var num = spl[spl.length-1]
     
     console.log(req.method + ' ' + req.url)
-    console.log(pag)
-    
     if(req.method == 'GET'){
-        if(parseInt(pag,10) < 449){
-            fs.readFile('data/doc' + pag + '.xml', (erro, dados)=>{
+        if((parseInt(num,10) > 0) && (parseInt(num,10) < 449)){
+            fs.readFile('data/doc' + num + '.xml', (erro, dados)=>{
                 if(!erro){
                     res.writeHead(200, {'Content-Type':'text/xml'}) 
                     res.write(dados);
                 }
                 else {
                     res.writeHead(200, {'Content-Type':'text/plain'}) 
-                    res.write('Erro na leitura do ficheiro ' + 'data/doc' + pag + '.xml...')
+                    res.write('Erro na leitura do ficheiro ' + 'data/doc' + num + '.xml...')
                 }
                 res.end()  
             });      
         }
-        else if(pag=="doc2html.xsl"){
+        else if(num=="doc2html.xsl"){
             fs.readFile('doc2html.xsl', (erro, dados)=>{
                 if(!erro){
                     res.writeHead(200, {'Content-Type':'text/xml'}) 
@@ -49,9 +47,11 @@ var myserver = http.createServer(function(req,res) {
             });
         }
         else{
+            res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'}) 
             res.end('Erro: Pedido não suportado [' + req.url + ']');
         }
     } else {
+        res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'}) 
         res.end('Erro: Método não suportado [' + req.method + ']');
     } 
 })
